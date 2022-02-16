9a6a8e88b9ab65214794b388c66d51af0208b447
Courtney Robichaud <44408770+cdrobich@users.noreply.github.com>
2022-02-16 09:47:06 -0500

    Made a few minor changes

--- Scripts/basic_analysis.R
+++ Scripts/basic_analysis.R
@@ -14,48 +14,48 @@ library(tidyverse) # loading tidyverse package for ggplot etc.
 
 sessionInfo() # good practice to copy and paste this after loading all packages
 #so that other users know the soft/hardware you are working with
+# can put this in your script or put it in the readme
 
-#R version 4.0.3 (2020-10-10)
+
+
+#R version 4.1.2 (2021-11-01)
 #Platform: x86_64-w64-mingw32/x64 (64-bit)
 #Running under: Windows 10 x64 (build 22000)
-
+#
 #Matrix products: default
-
+#
 #locale:
-#[1] LC_COLLATE=English_Canada.1252  LC_CTYPE=English_Canada.1252   
-#[3] LC_MONETARY=English_Canada.1252 LC_NUMERIC=C                   
-#[5] LC_TIME=English_Canada.1252    
+#[1] LC_COLLATE=English_Canada.1252  LC_CTYPE=English_Canada.1252    LC_MONETARY=English_Canada.1252
+#[4] LC_NUMERIC=C                    LC_TIME=English_Canada.1252    
+#system code page: 65001
 
 #attached base packages:
 #[1] stats     graphics  grDevices utils     datasets  methods   base     
 
 #other attached packages:
-#[1] forcats_0.5.1   stringr_1.4.0   dplyr_1.0.8     purrr_0.3.4    
-#[5] readr_2.1.2     tidyr_1.2.0     tibble_3.1.6    ggplot2_3.3.5  
-#[9] tidyverse_1.3.1 remotes_2.4.2  
+#[1] forcats_0.5.1        stringr_1.4.0        dplyr_1.0.8          purrr_0.3.4         
+#[5] readr_2.1.2          tidyr_1.2.0          tibble_3.1.6         ggplot2_3.3.5       
+#[9] tidyverse_1.3.1      palmerpenguins_0.1.0 remotes_2.4.2       
 
 #loaded via a namespace (and not attached):
-#[1] Rcpp_1.0.8       cellranger_1.1.0 pillar_1.7.0     compiler_4.0.3  
-#[5] dbplyr_2.1.1     tools_4.0.3      jsonlite_1.7.3   lubridate_1.8.0 
-#[9] lifecycle_1.0.1  gtable_0.3.0     pkgconfig_2.0.3  rlang_1.0.1     
-#[13] reprex_2.0.1     rstudioapi_0.13  DBI_1.1.2        cli_3.2.0       
-#[17] haven_2.4.3      xml2_1.3.3       withr_2.4.3      httr_1.4.2      
-#[21] fs_1.5.2         generics_0.1.2   vctrs_0.3.8      hms_1.1.1       
-#[25] grid_4.0.3       tidyselect_1.1.1 glue_1.6.1       R6_2.5.1        
-#[29] fansi_1.0.2      readxl_1.3.1     tzdb_0.2.0       modelr_0.1.8    
-#[33] magrittr_2.0.2   backports_1.4.1  scales_1.1.1     ellipsis_0.3.2  
-#[37] rvest_1.0.2      assertthat_0.2.1 colorspace_2.0-2 utf8_1.2.2      
-#[41] stringi_1.7.6    munsell_0.5.0    broom_0.7.12     crayon_1.5.0  
+#[1] Rcpp_1.0.8       cellranger_1.1.0 pillar_1.7.0     compiler_4.1.2   dbplyr_2.1.1    
+#[6] tools_4.1.2      lubridate_1.8.0  jsonlite_1.7.3   lifecycle_1.0.1  gtable_0.3.0    
+#[11] pkgconfig_2.0.3  rlang_1.0.1      reprex_2.0.1     rstudioapi_0.13  cli_3.2.0       
+#[16] DBI_1.1.2        curl_4.3.2       haven_2.4.3      xml2_1.3.3       withr_2.4.3     
+#[21] httr_1.4.2       fs_1.5.2         generics_0.1.2   vctrs_0.3.8      hms_1.1.1       
+#[26] grid_4.1.2       tidyselect_1.1.1 glue_1.6.1       R6_2.5.1         fansi_1.0.2     
+#[31] readxl_1.3.1     tzdb_0.2.0       modelr_0.1.8     magrittr_2.0.2   backports_1.4.1 
+#[36] scales_1.1.1     ellipsis_0.3.2   rvest_1.0.2      assertthat_0.2.1 colorspace_2.0-2
+#[41] utf8_1.2.2       stringi_1.7.6    munsell_0.5.0    broom_0.7.12     crayon_1.5.0    
 
 
 # Create data ---------------------------------------------------------------
 
 data(penguins, package = "palmerpenguins")
 
-write.csv(penguins_raw, "Raw data/penguins_raw.csv",
-          row.names = FALSE)
+write.csv(penguins_raw, "Raw data/penguins_raw.csv", row.names = FALSE)
 
-write.csv(penguins,"Data/penguins.csv", row.names = FALSE)
+write.csv(penguins,"Data/penguins.csv",row.names = FALSE)
 
 
 # Load data ---------------------------------------------------------------
@@ -63,6 +63,7 @@ write.csv(penguins,"Data/penguins.csv", row.names = FALSE)
 pen.data <- read.csv("Data/penguins.csv")
 
 str(pen.data) # look at data types (e.g., factor, character)
+colnames(pen.data) # look at the column names
 
 # check for bullshit
 head(pen.data) # first few rows of the start of the data
@@ -70,16 +71,16 @@ tail(pen.data) # last few rows at the end
 
 
 pairs(pen.data[,c(3:6,8)]) # pairwise correlation plot of numeric columns
-# [row, column] and we want columns 3:6 and 8 which are the numeric ones
+# [row, column] and we want columns 3:6 and 8 which are the numeric variables
+?pairs # will give you information about the function
+
 
 hist(pen.data$body_mass_g)  # make a histogram    
+?hist
+
 
 boxplot(pen.data$body_mass_g ~ pen.data$species) # boxplot of body mass x species
-
-
-# do you see any outliers?
-# "?" and your function will give you information e.g., "?boxplot"
-
+?boxplot
 
 
 # Save boxplot as pdf in base R -------------------------------------------
@@ -104,7 +105,8 @@ pen_fig <- pen.data %>% # calling on the data
   ggplot(aes(y = body_mass_g, x = sex, # aesthetic: y = body mass, x = sex
              colour = sex)) + # colour violin plots by sex
   facet_wrap(~species) + # each species will have it's own plot
-  geom_violin(trim = FALSE) + # violin plot, turn off trim the edges
+  geom_violin(trim = FALSE, # violin plot, turn off trim the edges
+              lwd = 1) + # make the lines thick
   theme_bw() + # black and white background theme
   theme(text = element_text(size = 12), # change the text size
         axis.text.x = element_text(size = 12),
@@ -112,7 +114,10 @@ pen_fig <- pen.data %>% # calling on the data
         strip.text = element_text(size=12),
         legend.position = "none") + # remove the legend
   labs(y = "Body Mass (g)", # specify labels on axes
-       x = " ")
+       x = " ") +
+  scale_colour_manual(values = c("black", "darkgrey"))
+
+pen_fig
 
 
 ggsave("Output/pen_fig.jpeg", pen_fig, # save figure to output
